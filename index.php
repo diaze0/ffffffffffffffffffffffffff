cfe8011bcb7fad34b594741a14026206266f1d01
diaze0 <92938629+diaze0@users.noreply.github.com>
2022-06-20 16:59:14 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Portfolio website</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+	<!----hero Section start---->
+
+	<div class="hero">
+		<nav>
+			<h2 class="logo">fruit de la passion<span>lio</span></h2>
+			<ul>
+			    <li><a href="#">Qui sommes nous</a></li>
+				<li><a href="#">Origines</a></li>
+				<li><a href="#">Recette</a></li>
+				<li><a href="#">Béréfice</a></li>
+				
+			</ul>
+			<a href="#" class="btn">Commander</a>
+		</nav>
+
+		<div class="content">
+			<h4>la passion</h4>
+			<h1>fruit de <span>la passion</span></h1>
+			<h3>fruit.</h3>
+			<div class="newslatter">
+				
+			</div>
+		</div>
+	</div>
+
+	<!----About section start---->
+	<section class="about">
+		<div class="main">
+			<img src="img/dd.jpg">
+			<div class="about-text">
+				<h2>Qui sommes nous</h2>
+				<h5>agriculture <span>biologique</span></h5>
+				<p>Nous sommes une ferme pour la culture de fruits et nous cultivons de manière naturelle sans produits chimiques vivants.  Nous nous soucions de la qualité des produits et de la sécurité des clients.</p>
+				
+			</div>
+		</div>
+	</section>
+
+	<!------footer start--------->
+	<footer>
+		<p>Tahmid Ahmed</p>
+		<p>For more HTML, CSS, and coding tutorial - please click on the link below to subscribe to my channel:</p>
+		<div class="social">
+			<a href="#"><i class="fab fa-facebook-f"></i></a>
+			<a href="#"><i class="fab fa-instagram"></i></a>
+			<a href="#"><i class="fab fa-dribbble"></i></a>
+		</div>
+		<p class="end">CopyRight By Tahmid Ahmed</p>
+	</footer>
+</body>
+</html>
